63b54069ec6feebbb8ea36046e02c3b602fcd192
khembach <3229704+khembach@users.noreply.github.com>
2020-12-21 14:57:13 +0100

    keep metadata in gene annotation and change barplot colors

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -14,7 +14,7 @@ library(GenomicFeatures)
 #' @importFrom GenomicFeatures makeTxDbFromGRanges
 #'
 #' @examples
-prepare_anno <- function(gtf){
+prepare_anno_old <- function(gtf){
   exon <- gtf[gtf$type == "exon"] %>% unique
   gene <- gtf[gtf$type == "gene"] %>% unique
   five_utr <- gtf[gtf$type == "five_prime_utr"] %>% unique
@@ -35,6 +35,60 @@ prepare_anno <- function(gtf){
                                          anno[["five_prime_utr"]]))
   anno
 }
+
+
+
+
+
+#' Prepare gtf annotation
+#'
+#' Split GTF annotation into intron, exon, 3'UTR and 5'UTR. The exonic parts 
+#' that overlap with UTRs are counted as UTRs! Intronic parts that overlap 
+#' with exons or UTRs are not considered introns.
+#'
+#' @param gtf GRanges object
+#'
+#' @return GRangesList with exon, intron, 3'UTR and 5'UTR annotation
+#' @export
+#' 
+#' @importFrom GenomicFeatures makeTxDbFromGRanges
+#'
+#' @examples
+prepare_anno <- function(gtf){
+  exon <- gtf[gtf$type == "exon"] %>% unique
+  gene <- gtf[gtf$type == "gene"] %>% unique
+  five_utr <- gtf[gtf$type == "five_prime_utr"] %>% unique
+  three_utr <- gtf[gtf$type == "three_prime_utr"] %>% unique
+  
+  ## We remove all 3' and 5' UTR regions that overlap with any exons
+  exon_utr <- GenomicRanges::setdiff(exon, three_utr)
+  exon_unique <- GenomicRanges::setdiff(exon_utr, five_utr) %>% unique
+  ## copy metadata information from original exons
+  olaps <- findOverlaps(exon_unique, exon)
+  idx <- which(!duplicated(queryHits(olaps))) ## take the first match
+  mcols(exon_unique) <- mcols(exon[subjectHits(olaps)[idx]])
+  anno <- GRangesList(gene = gene, exon = exon_unique, three_prime_utr = three_utr, 
+                      five_prime_utr = five_utr)
+  ## intron annotation
+  txdb <- makeTxDbFromGRanges(gtf)
+  introns <- unlist(intronsByTranscript(txdb))
+  ## match intron metadata
+  ## add one nucleotide upstream of the exon to allow overlap with intron
+  olaps <- findOverlaps(introns, resize(exon, width(exon) +1, fix="end"))
+  idx <- which(!duplicated(queryHits(olaps))) ## take the first match
+  mcols(introns) <- mcols(exon[subjectHits(olaps)[idx]])
+  
+  ## remove the intronic parts that overlap with exons from other transcripts
+  anno[["intron"]] <- GenomicRanges::setdiff(introns, c(anno[["exon"]], 
+                                                        anno[["three_prime_utr"]], 
+                                                        anno[["five_prime_utr"]]))
+  olaps <- findOverlaps(anno[["intron"]], introns)
+  idx <- which(!duplicated(queryHits(olaps))) ## take the first match
+  mcols(anno[["intron"]]) <- mcols(introns[subjectHits(olaps)[idx]])
+  anno
+}
+
+
 
 
 #' Peak location barplot
@@ -220,12 +274,115 @@ peak_gene_region_barplot_split <- function(peaks, anno) {
                            limits = c("unchanged", "down_8h", "up_8h", 
                                       "down_12h", "up_12h", "down_24h", "up_24h"),
                            name = "peaks in genes with RNA stability change") +
+          # scale_fill_manual(labels = c("exon", "5' UTR", "3' UTR", "intron"), 
+          #                   values = c("exon" = "#9C3B8C", 
+          #                              "five_prime_utr" = "#43BB5F",
+          #                              "three_prime_utr" = "cyan4", 
+          #                              "intron" = "#7BAFDE")) +
           scale_fill_manual(labels = c("exon", "5' UTR", "3' UTR", "intron"), 
-                            values = c("exon" = "#9C3B8C", 
-                                       "five_prime_utr" = "#43BB5F",
-                                       "three_prime_utr" = "cyan4", 
-                                       "intron" = "#7BAFDE")) +
+                            values = c("exon" = "#736EAD", 
+                                       "five_prime_utr" = "#F19903",
+                                       "three_prime_utr" = "#F3DB3C", 
+                                       "intron" = "#018C89")) +              
           guides(fill = guide_legend(nrow=2, byrow = TRUE)) 
 }
 
+
+peak_gene_region_barplot_6mo <- function(peaks, anno) {
+  olap_len <- list()
+  for (sample in names(peaks)){
+    olap_len[[sample]] <- lapply(anno, function(x) 
+      length(suppressWarnings(subsetByOverlaps(peaks[[sample]], x, type = "any"))))
+    names(olap_len[[sample]]) <- names(anno)
+  } 
+  
+  df <- as.data.frame( t( 
+    cbind( 
+      sapply(olap_len, as.data.frame)) 
+  ) )
+  
+  df$peaks <- rownames(df)
+  df[,names(df) != "peaks"] <- apply(df[,names(df) != "peaks"], 2, as.integer)
+  
+  df <- df %>% gather(key = "annotation", value = "peak_number", -peaks)
+  df <- df[df$annotation != "gene",]
+  df$percentage <- df$peak_number / sapply(df$peaks, function(x) 
+    sum(df[df$peaks == x, "peak_number"]) ) * 100
+  
+  ## reorder the annotation factor levels
+  df$annotation <- factor(df$annotation, 
+                          levels = c("exon", "five_prime_utr", 
+                                     "three_prime_utr", "intron"))
+  print(df)
+  ## stacked barplot with the percentage of reads in the different gene regions
+  ggplot(df, aes(x = peaks, y = percentage, fill = annotation)) +
+    geom_col(position = "stack") +
+    theme_bw() +
+    theme(text = element_text(size = 20),
+          axis.text.x = element_text(angle = 45, hjust = 1))  +
+    scale_x_discrete(breaks= c("unchanged", "accumulated", "decreased"), 
+                     labels= c("unchanged", "accumulated", "decreased"),
+                     limits = c("unchanged", "accumulated", "decreased"),
+                     name = "peaks in genes with RNA level change at 6 months") +
+    scale_fill_manual(labels = c("exon", "5' UTR", "3' UTR", "intron"), 
+                      values = c("exon" = "#736EAD", 
+                                 "five_prime_utr" = "#F19903",
+                                 "three_prime_utr" = "#F3DB3C", 
+                                 "intron" = "#018C89")) 
+}
+
+
+
+
+peak_gene_region_barplot_combined <- function(peaks, anno) {
+  olap_len <- list()
+  for (sample in names(peaks)){
+    olap_len[[sample]] <- lapply(anno, function(x) 
+      length(suppressWarnings(subsetByOverlaps(peaks[[sample]], x, type = "any"))))
+    names(olap_len[[sample]]) <- names(anno)
+  } 
+  
+  df <- as.data.frame( t( 
+    cbind( 
+      sapply(olap_len, as.data.frame)) 
+  ) )
+  
+  df$peaks <- rownames(df)
+  df[,names(df) != "peaks"] <- apply(df[,names(df) != "peaks"], 2, as.integer)
+  
+  df <- df %>% gather(key = "annotation", value = "peak_number", -peaks)
+  df <- df[df$annotation != "gene",]
+  df$percentage <- df$peak_number / sapply(df$peaks, function(x) 
+    sum(df[df$peaks == x, "peak_number"]) ) * 100
+  
+  ## reorder the annotation factor levels
+  df$annotation <- factor(df$annotation, 
+                          levels = c("exon", "five_prime_utr", 
+                                     "three_prime_utr", "intron"))
+  print(df)
+  ## stacked barplot with the percentage of reads in the different gene regions
+  g <- ggplot(df, aes(x = peaks, y = percentage, fill = annotation)) +
+    geom_col(position = "stack") +
+    theme_bw() +
+    theme(text = element_text(size = 20),  legend.position = "bottom", 
+          legend.direction="horizontal", legend.box = "horizontal",
+          axis.text.x = element_text(angle = 45, hjust = 1))  +
+    scale_x_discrete(breaks= c("unchanged_stability", "down_8h", "up_8h", 
+                               "down_12h", "up_12h", "down_24h", "up_24h", 
+                               "unchanged_6months", "decreased", "accumulated"), 
+                     labels= c("unchanged_stability", "down_8h", "up_8h", 
+                               "down_12h", "up_12h", "down_24h", "up_24h", 
+                               "unchanged_6months", "decreased", "accumulated"),
+                     limits = c("unchanged_stability", "down_8h", "up_8h", 
+                                "down_12h", "up_12h", "down_24h", "up_24h", 
+                                "unchanged_6months", "decreased", "accumulated"),
+                     name = "peaks in genes with RNA level change") +
+    scale_fill_manual(labels = c("exon", "5' UTR", "3' UTR", "intron"), 
+                      values = c("exon" = "#736EAD", 
+                                 "five_prime_utr" = "#F19903",
+                                 "three_prime_utr" = "#F3DB3C", 
+                                 "intron" = "#018C89")) +
+    guides(fill = guide_legend(nrow=1, byrow = TRUE)) 
+  list(df = df, plot = g)
+}
 
